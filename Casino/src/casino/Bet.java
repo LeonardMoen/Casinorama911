3604e3d96d89a965b5e2856437c11343f4486438
laua8572 <laua8572@STLX109.classroom.ycdsb.ca>
2018-05-23 12:14:41 -0400

    oooo, bets now

---
+++ Casino/src/casino/Bet.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package casino;
+
+/**
+ *
+ * @author laua8572
+ */
+public class Bet {
+    private double betAmount;
+    private int payout, odds;
+}
