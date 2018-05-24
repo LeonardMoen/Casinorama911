74efb21d1c9186bb8c9fb35b193d6acddfc7be13
laua8572 <laua8572@STLX109.classroom.ycdsb.ca>
2018-05-24 12:19:48 -0400

    add arraylist of nums bet on to player class

---
+++ Casino/src/casino/PlayerBet.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package casino;
+
+/**
+ *
+ * @author laua8572
+ */
+public class PlayerBet extends Player{
+    private Arraylist <Integer> numbers = new Arraylist <Integers>();
+    
+    public PlayerBet(String name) {
+        super(name);
+        
+    }
+    
+}
