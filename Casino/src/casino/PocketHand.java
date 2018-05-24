b4ae005702d1e259530ccdf2284bbf487aa316b2
Farzad <Farzad@10.112.14.195>
2018-05-24 11:23:58 -0400

    Hand for blackjack

--- Casino/src/casino/PocketHand.java
+++ Casino/src/casino/PocketHand.java
@@ -7,30 +7,39 @@ public class PocketHand {
     private ArrayList<Card> playerHand = new ArrayList<>();
 
     public PocketHand(Deck deck) {
-        for (int x = 0; x <= 2; x++) {
+        for (int x = 0; x < 2; x++) {
             playerHand.add(deck.getDeck().get(x));
             deck.getDeck().remove(x);
         }
     }
 
+    public ArrayList<Card> getPlayerHand() {
+        return playerHand;
+    }
+
     public void hitCard(Deck deck) {
         playerHand.add(deck.getDeck().get(0));
+        deck.getDeck().remove(0);
     }
 
     public boolean checkBust() {
         int check = 0;
-        for (int i = 0; i < playerHand.size();i++ ) {
+        for (int i = 0; i < playerHand.size(); i++) {
             check += playerHand.get(i).getValue();
         }
-        return check >21;
+        return check > 21;
+    }
+
+    public boolean checkSplit() {
+        return this.playerHand.get(0).getValue() == this.playerHand.get(1).getValue();
     }
 
     public boolean checkBlackJack() {
         int check = 0;
-        for (int i = 0; i < playerHand.size();i++ ) {
+        for (int i = 0; i < playerHand.size(); i++) {
             check += playerHand.get(i).getValue();
         }
-        return check ==21;
+        return check == 21;
     }
 //
 //    public boolean checkSuited() {
