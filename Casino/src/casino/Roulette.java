98cdda26d1907416fd82d92b25b60c3d88266c8c
laua8572 <laua8572@STLX109.classroom.ycdsb.ca>
2018-05-24 12:19:00 -0400

    ui stuff

--- Casino/src/casino/Roulette.java
+++ Casino/src/casino/Roulette.java
@@ -5,6 +5,11 @@
  */
 package casino;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Random;
+
 /**
  *
  * @author laua8572
@@ -12,10 +17,38 @@ package casino;
 public class Roulette {
 
     private Bet bet;
-    private int[][] board = new int [13][3];
+    private int[][] board = new int[13][3];
 
-    public void playRoulette() {
+    static InputStreamReader inStream = new InputStreamReader(System.in);
+    static BufferedReader stdin = new BufferedReader(inStream);
+    static Random rand = new Random();
+
+    public Roulette() {
+
+    }
+
+    public void playRoulette() throws IOException {
         createBoard(board);
+        Bet bet = new Bet();
+
+        System.out.println("Pick a bet type");
+        System.out.println("(1)Single\t(2)Split\t(3)Street\t(4)Double Street\t(5)Corner\t(6)High, Low, even, odd, black or red");
+        System.out.println("(7)Dozen or snake or Column");
+        int choice = Integer.parseInt(stdin.readLine());
+
+        System.out.println("How much do you want to bet?"); //need to substract bet from player chips
+        int amountBet = Integer.parseInt(stdin.readLine());
+
+        if (choice == 1) {
+            System.out.println("What num do you want to bet on?");  //place chip here (on board)
+            int betNum = Integer.parseInt(stdin.readLine());
+            
+        }
+
+    }
+    
+    public void Winner(){
+        System.out.println("You Won!!!!!!!!");
     }
 
     public static void createBoard(int[][] board) {
@@ -33,18 +66,18 @@ public class Roulette {
     public static void displayBoard(int[][] board) {
         for (int i = 0; i <= 12; i++) {
             for (int j = 0; j < 3; j++) {
-                System.out.print(board[i][j]+"\t");
+                System.out.print(board[i][j] + "\t");
             }
             System.out.println("");
         }
     }
-    
-    public static void checkWin(){
+
+    public static void checkWin() {
         //get odds from bet method and see if they win
     }
-    public static void payout(){
+
+    public static void payout() {
         //get pay ratio (odds-1), add to player's account
     }
-    
 
 }
