c86481b9a8f7426b5ae1c57ffb9d6b672817051e
laua8572 <laua8572@STLX109.classroom.ycdsb.ca>
2018-05-24 12:19:22 -0400

    bet odds

--- Casino/src/casino/Bet.java
+++ Casino/src/casino/Bet.java
@@ -10,10 +10,44 @@ package casino;
  * @author laua8572
  */
 public class Bet {
-    private double betAmount;
-    private int payout, odds;
     
-    //make methods of all types of bets, return a number for odds and payout multiplyer (payout is odds-1 to keep house advantage)
+    
+    //make methods of all types of bets, return a num of squares bet on (payout is (37/odds)-1 to keep house advantage)
    
+    public void Bet(){
+        
+    }
+    public int single(){
+        return 1;
+    }
+    public int splitH(){
+        return 2;       //check if it is horizontal in roulette class
+    }
+    public int splitV(){
+        return 4;       //check if it is vertical in roulette class
+    }
+    public int street(){
+        return 3;
+    }
+    public int doubleStreet(){
+        return 6;
+    }
+    public int corner(){    //check if it is a corner num in roulette class
+        return 4;
+    }
+    public int lowHighEvenOddRedBlack(){
+        return 18;
+    }
+    public int dozenOrSnakeOrColumn(){
+        return 12;
+    }
+
+    
+
+    
+    
+    
+    
+    
     
 }
