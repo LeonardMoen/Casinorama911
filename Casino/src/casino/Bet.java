fdbc2cdd80fe11844c9e3909fac2e208cfe1047d
Anthony <Anthony@192.168.0.27>
2018-05-27 01:33:41 -0400

    added choices

--- Casino/src/casino/Bet.java
+++ Casino/src/casino/Bet.java
@@ -5,49 +5,90 @@
  */
 package casino;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author laua8572
  */
 public class Bet {
-    
-    
-    //make methods of all types of bets, return a num of squares bet on (payout is (37/odds)-1 to keep house advantage)
-   
-    public void Bet(){
-        
-    }
-    public int single(){
-        return 1;
-    }
-    public int splitH(){
-        return 2;       //check if it is horizontal in roulette class
-    }
-    public int splitV(){
-        return 4;       //check if it is vertical in roulette class
-    }
-    public int street(){
-        return 3;
-    }
-    public int doubleStreet(){
-        return 6;
-    }
-    public int corner(){    //check if it is a corner num in roulette class
-        return 4;
-    }
-    public int lowHighEvenOddRedBlack(){
-        return 18;
-    }
-    public int dozenOrSnakeOrColumn(){
-        return 12;
+
+    private double amountBet;
+    private int betType, playerNum;
+    private ArrayList<Integer> numsBetOn = new ArrayList<Integer>();
+
+    public Bet(double amountBet, int betType, int playerNum, ArrayList<Integer> numsBetOn) {
+        this.amountBet = amountBet;
+        this.betType = betType;
+        this.playerNum = playerNum;
+        this.numsBetOn = numsBetOn;
     }
 
-    
+    public double getPayout() {
+        if (betType == 1) {//single
+            return 36;
+        } else if (betType == 2) {//split
+            return 18;
+        } else if (betType == 3) {//street
+            return 12;
+        } else if (betType == 4) {//corner
+            return 9;
+        } else if (betType >= 5 && betType <= 10) {
+            return 2;
+        } else if (betType >= 11 && betType <= 13) {
+            return 3;
+        } else if (betType >= 14 && betType <= 16) {
+            return 3;
+        }
+        return -1000;
+    }
 
-    
-    
-    
-    
-    
-    
+    public double getAmountBet() {
+        return amountBet;
+    }
+
+    public void setAmountBet(double amountBet) {
+        this.amountBet = amountBet;
+    }
+
+    public int getBetType() {
+        return betType;
+    }
+
+    public void setBetType(int betType) {
+        this.betType = betType;
+    }
+
+    public int getPlayerNum() {
+        return playerNum;
+    }
+
+    public void setPlayerNum(int playerNum) {
+        this.playerNum = playerNum;
+    }
+
+    //    public int single(){1
+//        return 1;
+//    }
+//    public int splitH(){2
+//        return 2;       //check if it is horizontal in roulette class
+//    }
+//    public int splitV(){3
+//        return 4;       //check if it is vertical in roulette class
+//    }
+//    public int street(){4
+//        return 3;
+//    }
+//    public int doubleStreet(){5
+//        return 6;
+//    }
+//    public int corner(){6    //check if it is a corner num in roulette class
+//        return 4;
+//    }
+//    public int lowHighEvenOddRedBlack(){
+//        return 18;
+//    }
+//    public int dozenOrSnakeOrColumn(){
+//        return 12;
+//    }
 }
