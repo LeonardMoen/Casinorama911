b5d18d19d89227d3cd66ed9f8702f4bd80ad5503
Anthony <Anthony@192.168.0.27>
2018-05-27 01:33:30 -0400

    added choices

--- Casino/src/casino/Roulette.java
+++ Casino/src/casino/Roulette.java
@@ -17,51 +17,293 @@ import java.util.Random;
  */
 public class Roulette {
 
-    private Bet bet;
-    private int[][] board = new int[13][3];
-
     static InputStreamReader inStream = new InputStreamReader(System.in);
     static BufferedReader stdin = new BufferedReader(inStream);
     static Random rand = new Random();
 
+    static ArrayList<Integer> blackNums = new ArrayList<Integer>();
+
     public Roulette() {
 
     }
+    
+    
 
     public void playRoulette(ArrayList<Player> players) throws IOException {
-        createBoard(board);
-        Bet bet = new Bet();
+
+        fillBlackNums();
+
+        ArrayList<Bet> bets = new ArrayList<Bet>();
 
         for (int i = 0; i < players.size(); i++) {
-            
+
             int choice = -1;
-            while (true) {
+            while (true) {  //allows  you to bet more than once
                 System.out.println("Pick a bet type");
-                System.out.println("(0)Exit\t(1)Single\t(2)Split\t(3)Street\t(4)Double Street\t(5)Corner");
-                System.out.println("(6)High, Low, even, odd, black or red\t(7)Dozen or snake or Column");
+                System.out.println("(0)Exit\t(1)Single\t(2)Split\t(3)Street\t(4)Corner");
+                System.out.println("(5)High\t(6)Low\t(7)even\t(8)odd\t(9)black");
+                System.out.println("(10)red\t(11)1st 12\t(12)2nd 12\t(13)3rd 12\t(14)Column 1\t(15)Column 2\t(16)Column 3");
                 choice = Integer.parseInt(stdin.readLine());
-                if(choice ==0){
+                if (choice == 0) {
                     break;
                 }
-
-                System.out.println("How much do you want to bet?"); //need to substract bet from player chips
-                int amountBet = Integer.parseInt(stdin.readLine());
-            }
-
-            if (choice == 1) {
-                System.out.println("What num do you want to bet on?");  //place chip here (on board)
-                int betNum = Integer.parseInt(stdin.readLine());
-
-                ArrayList<Integer> numsBetOn = new ArrayList<Integer>();
-                numsBetOn.add(betNum);
-
-                players.get(i).setNumsBetOn(numsBetOn);
-            }
-            if(choice ==2){
+                
+                choices(i, players, choice, bets);
                 
             }
         }
 
+    }
+    public void choices(int i, ArrayList<Player> players, int choice, ArrayList<Bet> bets) throws IOException{
+        if (choice == 1) {
+                    System.out.println("What num do you want to bet on?");  //place chip here (on board)
+                    int betNum = Integer.parseInt(stdin.readLine());
+
+                    if (betNum == 0) {
+                        System.out.println("You cannot bet on 0");
+                    } else {
+                        ArrayList<Integer> numsBetOn = new ArrayList<Integer>();
+                        numsBetOn.add(betNum);
+
+                        System.out.println("How much do you want to bet?");
+                        int amountBet = Integer.parseInt(stdin.readLine());
+
+                        players.get(i).setBet(amountBet);
+                        bets.add(new Bet(amountBet, 1, i, numsBetOn));
+                    }
+                }
+                if (choice == 2) {
+                    System.out.println("Pick you first number");
+                    int betNum1 = Integer.parseInt(stdin.readLine());
+                    int betNum2 = num2Split(betNum1);
+
+                    ArrayList<Integer> numsBetOn = new ArrayList<Integer>();
+                    for (int j = betNum1; j <= betNum2; j++) {
+                        numsBetOn.add(j);
+                    }
+
+                    System.out.println("How much do you want to bet?");
+                    int amountBet = Integer.parseInt(stdin.readLine());
+
+                    players.get(i).setBet(amountBet);
+                    bets.add(new Bet(amountBet, 2, i, numsBetOn));
+
+                }
+                if (choice == 3) {
+                    System.out.println("Pick the first number of the row you want");
+                    int betNum1 = Integer.parseInt(stdin.readLine());
+                    int betNum2 = betNum1 + 2;
+
+                    ArrayList<Integer> numsBetOn = new ArrayList<Integer>();
+                    for (int j = betNum1; j <= betNum2; j++) {
+                        numsBetOn.add(j);
+                    }
+
+                    System.out.println("How much do you want to bet?");
+                    int amountBet = Integer.parseInt(stdin.readLine());
+
+                    players.get(i).setBet(amountBet);
+                    bets.add(new Bet(amountBet, 3, i, numsBetOn));
+
+                }
+                if (choice == 4) {
+                    System.out.println("Pick the first number of the corner you want");
+                    int betNum1 = Integer.parseInt(stdin.readLine());
+
+                    if (betNum1 != 10 || betNum1 != 11 || betNum1 != 22 || betNum1 != 23) {
+                        System.out.println("that's not the 1st number of a corner");
+                    } else {
+                        int betNum2 = betNum1 + 4;
+
+                        ArrayList<Integer> numsBetOn = new ArrayList<Integer>();
+                        for (int j = betNum1; j <= betNum2; j++) {
+                            numsBetOn.add(j);
+                        }
+                        numsBetOn.remove(2);//remove 3rd num cuz its not part of the corner numbers
+
+                        System.out.println("How much do you want to bet?");
+                        int amountBet = Integer.parseInt(stdin.readLine());
+
+                        players.get(i).setBet(amountBet);
+                        bets.add(new Bet(amountBet, 4, i, numsBetOn));
+                    }
+                }
+                if (choice == 5) {
+                    ArrayList<Integer> numsBetOn = new ArrayList<Integer>();
+                    for (int j = 1; j <= 18; j++) {
+                        numsBetOn.add(j);
+                    }
+
+                    System.out.println("How much do you want to bet?");
+                    int amountBet = Integer.parseInt(stdin.readLine());
+
+                    players.get(i).setBet(amountBet);
+                    bets.add(new Bet(amountBet, 5, i, numsBetOn));
+                }
+                if (choice == 6) {
+                    ArrayList<Integer> numsBetOn = new ArrayList<Integer>();
+                    for (int j = 19; j <= 36; j++) {
+                        numsBetOn.add(j);
+                    }
+
+                    System.out.println("How much do you want to bet?");
+                    int amountBet = Integer.parseInt(stdin.readLine());
+
+                    players.get(i).setBet(amountBet);
+                    bets.add(new Bet(amountBet, 6, i, numsBetOn));
+                }
+                if (choice == 7) {
+                    ArrayList<Integer> numsBetOn = new ArrayList<Integer>();
+                    for (int j = 2; j <= 36; j += 2) {
+                        numsBetOn.add(j);
+                    }
+
+                    System.out.println("How much do you want to bet?");
+                    int amountBet = Integer.parseInt(stdin.readLine());
+
+                    players.get(i).setBet(amountBet);
+                    bets.add(new Bet(amountBet, 7, i, numsBetOn));
+                }
+                if (choice == 8) {
+                    ArrayList<Integer> numsBetOn = new ArrayList<Integer>();
+                    for (int j = 1; j <= 36; j += 2) {
+                        numsBetOn.add(j);
+                    }
+
+                    System.out.println("How much do you want to bet?");
+                    int amountBet = Integer.parseInt(stdin.readLine());
+
+                    players.get(i).setBet(amountBet);
+                    bets.add(new Bet(amountBet, 8, i, numsBetOn));
+                }
+                if (choice == 9) {
+
+                    System.out.println("How much do you want to bet?");
+                    int amountBet = Integer.parseInt(stdin.readLine());
+
+                    players.get(i).setBet(amountBet);
+                    bets.add(new Bet(amountBet, 9, i, blackNums));
+                }
+                if (choice == 10) {
+                    ArrayList<Integer> numsBetOn = new ArrayList<Integer>();
+
+                    for (int j = 1; j <= 36; j++) {
+                        if (!(blackNums.contains(j))) {//if black nums doesnt contain j, add it to numsBetOn
+                            numsBetOn.add(j);
+                        }
+                    }
+
+                    System.out.println("How much do you want to bet?");
+                    int amountBet = Integer.parseInt(stdin.readLine());
+
+                    players.get(i).setBet(amountBet);
+                    bets.add(new Bet(amountBet, 10, i, numsBetOn));
+                }
+
+                if (choice == 11) {
+                    ArrayList<Integer> numsBetOn = new ArrayList<Integer>();
+                    for (int j = 1; j <= 12; j++) {
+                        numsBetOn.add(j);
+                    }
+
+                    System.out.println("How much do you want to bet?");
+                    int amountBet = Integer.parseInt(stdin.readLine());
+
+                    players.get(i).setBet(amountBet);
+                    bets.add(new Bet(amountBet, 11, i, numsBetOn));
+                }
+                if (choice == 12) {
+                    ArrayList<Integer> numsBetOn = new ArrayList<Integer>();
+                    for (int j = 13; j <= 24; j++) {
+                        numsBetOn.add(j);
+                    }
+
+                    System.out.println("How much do you want to bet?");
+                    int amountBet = Integer.parseInt(stdin.readLine());
+
+                    players.get(i).setBet(amountBet);
+                    bets.add(new Bet(amountBet, 12, i, numsBetOn));
+                }
+                if (choice == 13) {
+                    ArrayList<Integer> numsBetOn = new ArrayList<Integer>();
+                    for (int j = 25; j <= 36; j++) {
+                        numsBetOn.add(j);
+                    }
+
+                    System.out.println("How much do you want to bet?");
+                    int amountBet = Integer.parseInt(stdin.readLine());
+
+                    players.get(i).setBet(amountBet);
+                    bets.add(new Bet(amountBet, 13, i, numsBetOn));
+                }
+                if (choice == 14) {
+                    ArrayList<Integer> numsBetOn = new ArrayList<Integer>();
+                    for (int j = 1; j <= 36; j += 3) {
+                        numsBetOn.add(j);
+                    }
+
+                    System.out.println("How much do you want to bet?");
+                    int amountBet = Integer.parseInt(stdin.readLine());
+
+                    players.get(i).setBet(amountBet);
+                    bets.add(new Bet(amountBet, 14, i, numsBetOn));
+                }
+                if (choice == 15) {
+                    ArrayList<Integer> numsBetOn = new ArrayList<Integer>();
+                    for (int j = 2; j <= 36; j += 3) {
+                        numsBetOn.add(j);
+                    }
+
+                    System.out.println("How much do you want to bet?");
+                    int amountBet = Integer.parseInt(stdin.readLine());
+
+                    players.get(i).setBet(amountBet);
+                    bets.add(new Bet(amountBet, 15, i, numsBetOn));
+                }
+                if (choice == 16) {
+                    ArrayList<Integer> numsBetOn = new ArrayList<Integer>();
+                    for (int j = 3; j <= 36; j += 3) {
+                        numsBetOn.add(j);
+                    }
+
+                    System.out.println("How much do you want to bet?");
+                    int amountBet = Integer.parseInt(stdin.readLine());
+
+                    players.get(i).setBet(amountBet);
+                    bets.add(new Bet(amountBet, 16, i, numsBetOn));
+                }
+    }
+
+    public void fillBlackNums() {
+
+        for (int j = 2; j <= 10; j += 2) {
+            blackNums.add(j);
+        }
+        for (int j = 20; j <= 28; j += 2) {
+            blackNums.add(j);
+
+        }
+        for (int j = 11; j <= 18; j += 2) {
+            blackNums.add(j);
+        }
+        for (int j = 29; j <= 36; j += 2) {
+            blackNums.add(j);
+        }
+    }
+
+    public int num2Split(int betNum1) throws IOException {
+        int betNum2 = -1;
+
+        while (true) {
+            System.out.println("Pick your second number");
+            if (betNum2 != betNum1 + 1 || betNum2 != betNum1 - 1 || betNum2 != betNum1 + 3 || betNum2 != betNum1 - 3) {
+                betNum2 = Integer.parseInt(stdin.readLine());
+                return betNum2;
+            } else {
+                System.out.println("You didn't enter a valid 2nd number. Try again");
+            }
+
+        }
     }
 
     public void Winner() {
@@ -76,25 +318,8 @@ public class Roulette {
         //get pay ratio (odds-1), add to player's account
     }
 
-    public static void createBoard(int[][] board) {
-        board[0][0] = 0;
-        int counter = 1;
-        for (int i = 1; i <= 12; i++) {
-            for (int j = 0; j < 3; j++) {
-                board[i][j] = counter;
-                counter++;
-            }
-        }
-        displayBoard(board);
-    }
 
-    public static void displayBoard(int[][] board) {
-        for (int i = 0; i <= 12; i++) {
-            for (int j = 0; j < 3; j++) {
-                System.out.print(board[i][j] + "\t");
-            }
-            System.out.println("");
-        }
-    }
+
+
 
 }
