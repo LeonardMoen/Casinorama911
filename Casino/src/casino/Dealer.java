648b77a4d814fa69431f67d22eb3556cc808a328
Farzad <Farzad@10.112.14.195>
2018-05-28 20:32:29 -0400

    Merge origin/master
    Conflicts:
    	Casino/src/casino/BlackjackJAVA.java
    	Casino/src/casino/Player.java
    	Casino/src/casino/PlayerBet.java

--- Casino/src/casino/Dealer.java
+++ Casino/src/casino/Dealer.java
@@ -4,6 +4,7 @@ public class Dealer {
 
     private PocketHand dealerHand;
     private int total;
+    private boolean bust = false;
 
     public Dealer(Deck deck) {
         this.dealerHand = new PocketHand(deck);
@@ -13,11 +14,24 @@ public class Dealer {
         return dealerHand;
     }
 
+    public boolean isBust() {
+        return bust;
+    }
+
+    public void setBust(boolean bust) {
+        this.bust = bust;
+    }
+
     public void setTotal() {
         this.total = 0;
         for (int i = 0; i < dealerHand.getPlayerHand().size(); i++) {
-            this.total = dealerHand.getPlayerHand().get(i).getWorth();
+            this.total += dealerHand.getPlayerHand().get(i).getWorth();
         }
+    }
+
+    public int getTotal() {
+        setTotal();
+        return total;
     }
 
     public void setDealerHand(PocketHand dealerHand) {
