720056d9ec46d13dae58dfc749aedb0a66f6f111
Farzad <Farzad@10.112.14.195>
2018-05-28 23:54:13 -0400

    Merge origin/master
    Conflicts:
    	Casino/src/casino/BlackjackJAVA.java
    	Casino/src/casino/Player.java
    	Casino/src/casino/PlayerBet.java

--- Casino/src/casino/Player.java
+++ Casino/src/casino/Player.java
@@ -3,56 +3,68 @@ package casino;
 import java.util.ArrayList;
 
 public class Player {
-    
+
     String name;
     int chips;
     ArrayList<PocketHand> pocketHand = new ArrayList<>();
     ArrayList<Integer> numsBetOn = new ArrayList<Integer>();
     Hand hand;
     int playerNum, total, bet, insuranceAmount;
-    boolean insurance = false, stay = false;
-    
+    boolean insurance = false, stay = false, naturalBlackJack;
+
     public Player(String name) {
         this.name = name;
         this.chips = 500;
     }
-    
+
+    public void setInsuranceAmount(int insuranceAmount) {
+        this.insuranceAmount = insuranceAmount;
+    }
+
+    public boolean isNaturalBlackJack() {
+        return naturalBlackJack;
+    }
+
+    public void setNaturalBlackJack(boolean naturalBlackJack) {
+        this.naturalBlackJack = naturalBlackJack;
+    }
+
     public int getInsuranceAmount() {
         return insuranceAmount;
     }
-    
+
     public void setInsuranceAmount() {
         this.insuranceAmount = bet / 2;
         chips = chips - insuranceAmount;
     }
-    
+
     public void setStay(boolean stay) {
         this.stay = stay;
     }
-    
+
     public boolean isStay() {
         return stay;
     }
-    
+
     public Player(String name, Deck deck) {
         this.name = name;
         this.chips = 500;
         this.pocketHand.add(new PocketHand(deck));
     }
-    
+
     public void setInsurance(boolean insurance) {
         this.insurance = insurance;
     }
-    
+
     public boolean isInsurance() {
         return insurance;
     }
-    
+
     public void setBet(int bet) {
         this.bet = bet;
         chips = chips - bet;
     }
-    
+
     public int setTotal(int n) {
         this.total = 0;
         int optionalTotal = 0;
@@ -71,7 +83,7 @@ public class Player {
             return 0;
         }
     }
-    
+
     public int getTotal(int n) {
         total = 0;
         for (int i = 0; i < pocketHand.get(n).getPlayerHand().size(); i++) {
@@ -79,66 +91,66 @@ public class Player {
         }
         return total;
     }
-    
+
     public int getBet() {
         return bet;
     }
-    
+
     public void ifSplit(Deck deck) {
         this.pocketHand.add(new PocketHand(deck, pocketHand.get(0).getPlayerHand().get(1).getValue(), pocketHand.get(0).getPlayerHand().get(1).getSuit()));
         this.pocketHand.remove(pocketHand.get(0).getPlayerHand().get(1));
         this.pocketHand.get(0).hitCard(deck);
     }
-    
+
     public void setPocketHand(Deck deck) {
         this.pocketHand.clear();
         this.pocketHand.add(new PocketHand(deck));
     }
-    
+
     public ArrayList<PocketHand> getPocketHand() {
         return pocketHand;
     }
-    
+
     public String getName() {
         return name;
     }
-    
+
     public int getChips() {
         return chips;
     }
-    
+
     public Hand getHand() {
         return hand;
     }
-    
+
     public int getPlayerNum() {
         return playerNum;
     }
-    
+
     public void setName(String name) {
         this.name = name;
     }
-    
+
     public void setChips(int chips) {
         this.chips = chips;
     }
-    
+
     public void payout(int amount) {
         this.setChips(chips + amount);
     }
-    
+
     public void setHand(Hand hand) {
         this.hand = hand;
     }
-    
+
     public void setPlayerNum(int playerNum) {
         this.playerNum = playerNum;
     }
-    
+
     public ArrayList<Integer> getNumsBetOn() {
         return numsBetOn;
     }
-    
+
     public void setNumsBetOn(ArrayList<Integer> numsBetOn) {
         this.numsBetOn = numsBetOn;
     }
