4e9cfc6d79441a96e7510788b147785918bc93c2
L19mo <L19mo@10.112.5.93>
2018-06-01 14:09:29 -0400

    Started AI class

---
+++ Casino/src/casino/AI.java
@@ -0,0 +1,19 @@
+
+package casino;
+
+public class AI extends Player{
+
+    
+    public AI(String name, int playerNum) {
+        super(name, playerNum);
+    }
+
+    public AI(String name, Deck deck) {
+        super(name, deck);
+    }
+    
+//    public int preFlopBetting(){
+//        PocketHand pocketHand = super.getPocketHand();
+//        
+//    }
+}
