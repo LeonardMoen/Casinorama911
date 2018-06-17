60b5f950bb60f7cab9c3a8e5a86a3651b5d92208
Farzad <Farzad@10.112.14.195>
2018-06-17 13:09:20 -0400

    Merge origin/master
    Conflicts:
    	Casino/src/casino/RouletteGraphics.java

--- Casino/src/casino/BlackJackGraphics.java
+++ Casino/src/casino/BlackJackGraphics.java
@@ -1,35 +1,50 @@
-
 package casino;
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class BlackJackGraphics extends Application {
-    
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
+        Button hit = new Button("Hit");
+        hit.setLayoutX(10);
+        hit.setLayoutY(10);
+        Button stay = new Button("Stay");
+        Button dDown = new Button("Double Down");
+        Button split = new Button("Split");
+
+        hit.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+                try {
+                    BlackjackJAVA.playerHit(0, 0);
+                } catch (IOException ex) {
+                    Logger.getLogger(BlackJackGraphics.class.getName()).log(Level.SEVERE, null, ex);
+                }
             }
         });
-        
+
         StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
+        root.getChildren().addAll(hit);
+
         Scene scene = new Scene(root, 300, 250);
-        
+
         primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
         primaryStage.show();
-    }  
+    }
 }
